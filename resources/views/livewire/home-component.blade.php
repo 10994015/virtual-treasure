9608e855560604c805cff797e1bac2aacabb7aa6
10994015 <a0938599191@gmail.com>
2025-12-07 18:13:45 +0800

    側視圖片預覽S

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -96,7 +96,7 @@
                 <div class="p-6 transition-all bg-card-white rounded-system-lg shadow-system hover:shadow-system-lg">
                     <div class="flex items-center justify-center w-full h-48 mb-4 overflow-hidden rounded-system-lg">
                         @if ($product->product_image)
-                            <img src="{{ $product->product_image }}" onerror="this.onerror=null; this.src='/storage/{{ $product->product_image  }}'; alt="{{ $product->product_name }}" class="object-contain max-w-full max-h-full">
+                            <img src="{{ $product->product_image }}" onerror="this.onerror=null; this.src='/storage/{{ $product->product_image  }}'; alt="{{ $product->product_image }}" class="object-contain max-w-full max-h-full">
                         @else
                             <div class="flex items-center justify-center w-full h-full bg-gray-200">
                                 <i class="text-gray-400 fas fa-image"></i>
